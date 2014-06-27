43b48cdc89b3111a1d589158c7799663cb54f771
Cliff Wells <cliff.wells@rentrakmail.com>
2014-06-27 10:14:26 -0700

    refactoring, make boolean arguments actual booleans, not 'yes/no'

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,42 +4,31 @@
 #
 # === Parameters
 # [*hosts_cache*]
-# Set to yes or no, should hosts be cached or not by nscd.
-# Default: yes
+# Boolean, should hosts be cached or not by nscd.
+# Default: false
 #
 # [*group_cache*]
-# Set to yes or no, should hosts be cached or not by nscd.
-# Default: no
-# 
+# Boolean, should hosts be cached or not by nscd.
+# Default: false
+#
 # [*passwd_cache*]
-# Set to yes or no, should hosts be cached or not by nscd.
-# Default: no
+# Boolean, should hosts be cached or not by nscd.
+# Default: false
 #
 # === Authors
 # Steve Traylen, CERN, <steve.traylen@cern.ch>
 #
-class nscd::config ($hosts_cache  = hiera('hosts_cache','yes'),
-                    $passwd_cache = hiera('passwd_cache','no'),
-                    $group_cache  = hiera('group_cache','no')
-                   ) {
-
-
-     if ! ( $hosts_cache in ['yes','no'] ) {
-        fail("hosts_cache must be yes or no")
-     }
-     if ! ( $passwd_cache in ['yes','no'] ) {
-        fail("hosts_cache must be yes or no")
-     }
-     if ! ( $group_cache in ['yes','no'] ) {
-        fail("hosts_cache must be yes or no")
-     }
-
-
-     file{'/etc/nscd.conf':
+class nscd::config (
+    $hosts_cache    = hiera('hosts_cache',    false),
+    $passwd_cache   = hiera('passwd_cache',   false),
+    $group_cache    = hiera('group_cache',    false),
+    $services_cache = hiera('services_cache', false)
+){
+    file { '/etc/nscd.conf':
         ensure  => present,
         content => template('nscd/nscd.conf.erb'),
         require => Package['nscd'],
         notify  => Service['nscd']
-     }
+    }
 }
 
